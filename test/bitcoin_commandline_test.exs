55cc9e4d33d9dd426531d083776046f5b97329e4
wagners32 <wagnererlike@gmail.com>
2022-12-14 09:09:36 -0300

    Added test with mock

--- test/bitcoin_commandline_test.exs
+++ test/bitcoin_commandline_test.exs
@@ -2,12 +2,24 @@ defmodule BitcoinCommandlineTest do
   use ExUnit.Case
   doctest BitcoinCommandline
 
+  import Mock
+
+  test "bitcoin USD price" do
+    with_mock :httpc, [request: fn(_url) -> 
+        {:ok, 
+          {
+            {'HTTP/1.1', 200, 'OK'}, 
+            [{'cache-control', 'public; max-age=60'},{'date', 'Wed, 14 Dec 2022 11:17:53 GMT'}], 
+            '{"USD": {"15m": 17442.90,"last": 17442.91,"buy": 17442.92,"sell": 17442.93,"symbol": "ARS"}}'
+          }
+        } 
+        end] do
+        assert 17442.92 == BitcoinCommandline.get_price("USD")
+    end
+  end
+
   test "ticker url" do
     assert BitcoinCommandline.bitcoin_url() == "https://blockchain.info/ticker"
   end
 
-  test "bitcoin usd price" do
-    assert 17442.92 == BitcoinCommandline.get_price("USD")
-  end
-  
 end
